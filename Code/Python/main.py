b297d9758af0203d61540f2a49b4409c4bf48a3b
antoinejulien <antoine.ju99@outlook.fr>
2021-02-20 16:27:11 -0500

    Added new folders

---
+++ Code/Python/main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+#################################################################
+#     This is the start of our main program.                    #
+#     Other files and folder might be added                     #
+#     later on.                                                 #
+#                                                               #
+#     All files are created by the Level-US                     #
+#     team and other information can be found                   #
+#     in at the github page :                                   #
+#     https://github.com/antoinejulien/Level-US_GRO400_H21      #
+#################################################################
+
+if __name__ == "__main__":
+
+    print("Code starts here")
