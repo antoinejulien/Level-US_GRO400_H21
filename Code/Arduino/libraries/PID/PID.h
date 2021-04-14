5212966c83f930d29316f7b7233958021dea473c
franktreppromo65 <tref2310@usherbrooke.ca>
2021-04-14 11:38:16 -0400

    added pid class

---
+++ Code/Arduino/libraries/PID/PID.h
@@ -0,0 +1,20 @@
+/*
+Level-US project
+Class to implement a PID on the platform angles
+@author Antoine Julien (Level-US team)
+@version 1.0 14/04/2021
+*/
+
+#ifndef PID_H_
+#define PID_H_
+
+#include <Arduino.h>
+
+class PID
+{
+    public:
+        PID(){};
+		
+};
+
+#endif //PID_H_
